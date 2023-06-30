d3df971e3712b2edd4b1cefdaf2db29ffe002655
arifakkermans <arif.akkermans@outlook.com>
2023-06-30 18:52:08 +0200

    chore: remove env from cdk app

--- cdk/bin/index.ts
+++ cdk/bin/index.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { SimpleBookStack } from '../lib/lambda-api-gateway-stack';
 
-const envEU  = { account: '658544069824', region: 'eu-west-1' };
 const app = new cdk.App();
 
-new SimpleBookStack(app, 'SimpleBookStack', { env: envEU });
+new SimpleBookStack(app, 'SimpleBookStack');
